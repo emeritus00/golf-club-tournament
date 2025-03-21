790d5b0b466c38d3ffa323a1e19dba32fb90945d
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2025-03-21 01:09:17 -0230

    member codes added

--- src/main/java/com/keyin/domain/member/Member.java
+++ src/main/java/com/keyin/domain/member/Member.java
@@ -1,4 +1,78 @@
 package com.keyin.domain.member;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+import java.time.LocalDate;
+
+@Entity
 public class Member {
+    @Id
+    @SequenceGenerator(name = "aircraft_sequence", sequenceName = "aircraft_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "aircraft_sequence")
+    private Long id;
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private LocalDate startDate;
+    private int membershipDuration;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getMembershipDuration() {
+        return membershipDuration;
+    }
+
+    public void setMembershipDuration(int membershipDuration) {
+        this.membershipDuration = membershipDuration;
+    }
 }
--- src/main/java/com/keyin/domain/member/MemberRepository.java
+++ src/main/java/com/keyin/domain/member/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.keyin.domain.member;
 
-public interface MemberRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDate;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+    public Member findByNameContainingIgnoreCase(String name);
+    public Member findByPhoneNumber(String phoneNumber);
+    public Member findByStartDate(LocalDate startDate);
 }
